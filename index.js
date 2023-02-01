61179928f7790bf7b521206252a345a238ec1da1
Charvit123 <67510331+Charvit123@users.noreply.github.com>
2023-02-01 16:15:57 +0530

    added fakerApi

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-const express=require('express');
+import { faker } from '@faker-js/faker/locale/es_MX';
+import express, { query } from 'express'
+import _ from 'lodash'
 
 const app=express();
+
 app.listen(3000,()=>{
     console.log("app is started on 3000");
 });
 
+app.get("/address",(req,res)=>{
+    res.send(
+        _.times(10,()=>{
+            return {
+                "houseNo":faker.address.buildingNumber(),
+                "street":faker.address.street(),
+                "city":faker.address.city(),
+                "pincode":faker.address.zipCode(),
+                "country":faker.address.country(),
+        }})
+    );
+})
 
-users=[
-    {
-    "id":1,
-    "username":"Charvit",
-    "address":"gujrat",
-    },
-    {
-    "id":2,
-    "username":"Shubham",
-    "address":"Pune",   
-    }
-]
-
-app.get("/users",(req,res)=>{
-    res.json(users);
+app.get("/pokemon",(req,res)=>{
+    const param = req.query.count ?? 10;
+    console.log(param)
+    res.send(
+        _.times(param,()=>{
+            return {
+                "id":faker.datatype.uuid(),
+                "name":faker.animal.type(),
+                "power":faker.science.chemicalElement().name,
+                "imgUrl":faker.image.animals(),
+                "weight":faker.datatype.number(),
+                "height":faker.datatype.number(),
+        }})
+    );
 })
